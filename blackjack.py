af93e9e7f1e20fa3ebfb8bb17eea1c7784aed527
Zims <zims@inbox.lv>
2020-03-17 19:30:14 +0200

    mixup

--- blackjack.py
+++ blackjack.py
@@ -120,7 +120,6 @@ def show_some(player,dealer):
     print("\nDealer's Hand:")
     print(" <card hidden>")
     print('',dealer.cards[1])  
-    print("\nPlayer's Hand:", *player.cards, sep='\n ')
     print("Player's Hand =",player.value)
 
     
---
+++ k3.py
@@ -0,0 +1,6 @@
+my_nums=[5,7,2,3,4,1,6]
+print(f'Given list {my_nums}')
+my_nums.sort()
+# print(my_nums)
+reversed = my_nums[::-1]
+print(f'The third biggest num is {reversed[2]}')
